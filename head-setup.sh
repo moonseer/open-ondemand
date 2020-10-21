c630fb67e9bd00218597473f0f2cfa8d3d165fde
Curtis Wilson <cuwilson@microsoft.com>
2020-10-21 16:24:47 -0400

    commit on main

--- head-setup.sh
+++ head-setup.sh
@@ -16,3 +16,11 @@ systemctl restart sshd
 # Set up Host naming
 hostnamectl set-hostname head
 cp -f hosts /etc/hosts
+
+# Set up Fileshare mount 
+yum install cifs-utils -y
+useradd -u 5000 svc_ood
+groupadd -g 6000 share_ood
+usermod -G share_ood -a ood
+mkdir /ood_home
+mount.cifs \\\\192.168.2.215\\ood\\home /ood_home -o guest,uid=1000,iocharset=utf8,file_mode=0777,dir_mode=0777,noperm
