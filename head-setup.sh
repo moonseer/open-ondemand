cec61fcd0a33f443c5e8f073c0da8ed86fb04a13
Curtis Wilson <cuwilson@microsoft.com>
2020-10-21 16:39:31 -0400

    commit on main

--- head-setup.sh
+++ head-setup.sh
@@ -13,10 +13,6 @@ echo -n "ood" | passwd --stdin ood
 sed -i 's/^PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
 
-# Set up Host naming
-hostnamectl set-hostname head
-cp -f hosts /etc/hosts
-
 # Set up Fileshare mount 
 yum install cifs-utils -y
 useradd -u 5000 svc_ood
@@ -24,3 +20,8 @@ groupadd -g 6000 share_ood
 usermod -G share_ood -a ood
 mkdir /ood_home
 mount.cifs \\\\192.168.2.215\\ood\\home /ood_home -o guest,uid=1000,iocharset=utf8,file_mode=0777,dir_mode=0777,noperm
+
+# Set up Host naming
+hostnamectl set-hostname head
+cp -f /ood_home/hosts /etc/hosts
+
